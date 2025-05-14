5a6955a1dd986b68f10807255372584de9946769
compli <shirothegamer69@gmail.com>
2025-05-14 10:42:40 +0530

    navbar logo fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Logo: React.FC = () => {
-  return (
-    <div className="flex items-center">
-      <img src="/favicon.svg" alt="Mehta Finvest Logo" className="h-8 w-8 mr-2" />
-      <div className="flex flex-col">
-        <span className="text-secondary-800 font-heading font-bold text-2xl tracking-wide">MEHTA</span>
-        <span className="text-secondary-600 font-heading text-base tracking-[0.2em] -mt-1">FINVEST</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center -mt-3">
+			<img src="/favicon.svg" alt="Mehta Finvest Logo" className="h-12 w-12 " />
+			<div className="flex flex-col mt-4 ">
+				<span className="text-[#58595b] font-heading font-bold text-xl tracking-wide">
+					MEHTA
+				</span>
+				<span className="text-[#58595b] font-heading text-base text-sm tracking-[0.2em] -mt-1">
+					FINVEST
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Logo;
